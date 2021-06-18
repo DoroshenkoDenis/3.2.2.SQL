eb22cf7d0d082e2c383864713f03dce841e63004
Denis Doroshenko <ddtlt1984@gmail.com>
2021-06-18 17:04:01 +0400

    first

---
+++ src/test/java/ru/netology/web/test/UITest.java
@@ -0,0 +1,44 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.data.SQLSetter;
+import ru.netology.web.page.LoginPage;
+import ru.netology.web.page.VerificationPage;
+
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UITest {
+    LoginPage loginPage = new LoginPage();
+    VerificationPage verificationPage = new VerificationPage();
+
+    DataHelper.AuthInfo authInfo1 = DataHelper.getAuthInfo("vasya", "qwerty123");
+    DataHelper.AuthInfo authInfo2 = DataHelper.getAuthInfo("petya", "123qwerty");
+
+    DataHelper.BadPassword badPassword = DataHelper.getBadPassword("en");
+    String status = "blocked";
+    String dashBoardHeaderText = "  Личный кабинет";
+
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @AfterAll
+    static void clean() {
+        SQLSetter.dropDataBase();
+    }
+
+    @Test
+    void shouldLogin() {
+        loginPage
+                .validLogin(authInfo2)
+                .verify(authInfo2)
+                .shouldBeVisible(dashBoardHeaderText);
+    }
+
+}
